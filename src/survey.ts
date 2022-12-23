963b5638e2edb1c28050b0e079980ac2fe485eb1
WillRazorFace <willrazorface90s@gmail.com>
2022-12-23 10:04:02 -0300

    Decorator Autobind adicionado

--- src/survey.ts
+++ src/survey.ts
@@ -1,3 +1,20 @@
+function Autobind(target: any, methodName: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+
+    const adjDescriptor = {
+        configurable: true,
+        enumerable: false,
+
+        get() {
+            const boundFn = originalMethod.bind(this);
+            
+            return boundFn
+        }
+    }
+
+    return adjDescriptor;
+}
+
 class surveyForm {
     constructor(
         private formElement: HTMLFormElement,
@@ -16,6 +33,7 @@ class surveyForm {
         else this.renderSection('previous');
     }
 
+    @Autobind
     private renderSection(render: 'next' | 'previous') {
         const section = render === 'next' ? this.currentSection + 1 : this.currentSection - 1;
 
